86e1765c192606eb08a010cdcf0ad07dd9525e47
Johannes Filter <filter@Johanness-MacBook-Pro.local>
2015-08-24 00:12:18 +0200

    allow more zooming

--- app.js
+++ app.js
@@ -74,8 +74,8 @@ window.onload = function() {
   cartodb.createVis('map1', 'https://filter.cartodb.com/api/v2/viz/4ad72676-4835-11e5-b84d-0e0c41326911/viz.json', {"zoom": 8, "center_lat": 53.897441, "center_lon": 12.606970})
       .done(function(vis, layers) {
            vis.map.set({
-          minZoom: 7,
-          maxZoom: 9
+          minZoom: 6,
+          maxZoom: 10
         });
       })
       .error(function(err) {
@@ -85,8 +85,8 @@ window.onload = function() {
   cartodb.createVis('map2', 'https://filter.cartodb.com/api/v2/viz/570f0a04-4843-11e5-b25e-0e018d66dc29/viz.json', {"zoom": 8, "center_lat": 53.897441, "center_lon": 12.606970})
       .done(function(vis, layers) {
            vis.map.set({
-          minZoom: 7,
-          maxZoom: 9
+          minZoom: 6,
+          maxZoom: 10
         });
       })
       .error(function(err) {
@@ -95,8 +95,8 @@ window.onload = function() {
   cartodb.createVis('map3', 'https://filter.cartodb.com/api/v2/viz/3fd97510-4846-11e5-b9e2-0e853d047bba/viz.json', {"zoom": 8, "center_lat": 53.897441, "center_lon": 12.606970})
       .done(function(vis, layers) {
            vis.map.set({
-          minZoom: 7,
-          maxZoom: 9
+          minZoom: 6,
+          maxZoom: 10
         });
       })
       .error(function(err) {
